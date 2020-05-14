7d2ffe984d5413184f53d0c91cfd50cadc37cd63
snychka <stefannychka@gmail.com>
2020-05-13 21:19:26 -0600

    cannot pass 2nd-last task

--- authorization-server.js
+++ authorization-server.js
@@ -54,6 +54,87 @@ app.use(bodyParser.urlencoded({ extended: true }))
 Your code here
 */
 
+app.get('/authorize', (req, r) => {
+  let ci = req.query.client_id;
+  if (!clients[ci]) {
+    r.status(401).end();
+    return;
+  } 
+
+  let requested_scopes = req.query.scope.split(' ');
+  if (!containsAll(clients[ci].scopes, requested_scopes)) {
+    r.status(401).end();
+    return;
+  }
+
+  let rs = randomString();
+  requests[rs] = req.query;
+
+  let page = "login";
+  let params = {
+    client : clients[ci],
+    scope : req.query.scope,
+    requestId: rs
+  };
+  r.render(page, params)
+
+  r.status(200).end();
+
+});
+
+app.post('/approve', (req, res) => {
+  /*
+  console.log('req.body.password: ' + req.body.password)
+  console.log('req.body.username: ' + req.body.password)
+  console.log('req.body.password: ' + req.body.password); !== users[req.body.username]));
+  */
+  //console.log((req.body.password !== users[req.body.userName]));
+  if (req.body.password !== users[req.body.userName]) {
+    res.status(401).end();
+    return;
+  }
+
+  if (!requests[req.body.requestId]) {
+    res.status(401).end();
+    return;
+  }
+  let cr = requests[req.body.requestId];
+  delete requests[req.body.requestId];
+
+  let rs = randomString();
+  authorizationCodes[rs] = {
+    clientReq: cr,
+    userName: req.body.userName
+  };
+
+  res.redirect(cr.redirect_uri + '?code=' + encodeURIComponent(rs) + '&state=' + cr.state );
+
+  res.status(200).end();
+});
+
+app.post('/token', (req, res) => {
+  if(!req.headers.authorization) {
+    res.status(401).end();
+    return;
+  }
+
+  let {clientId, clientSecret} = decodeAuthCredentials(req.headers.authorization);
+  if(clients[clientId] !== clientSecret) {
+    res.status(401).end();
+    return;
+  }
+
+  //if(!req.body.code || !authorizationCodes[req.body.code]) {
+  if(!authorizationCodes[req.body.code]) {
+    res.status(401).end();
+    return;
+  }
+  let ac =authorizationCodes[req.body.code];
+  delete authorizationCodes[req.body.code];
+
+
+});
+
 const server = app.listen(config.port, "localhost", function () {
 	var host = server.address().address
 	var port = server.address().port
